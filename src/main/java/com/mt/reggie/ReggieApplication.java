c94d25b6e1259fed4d9b5ddd8681fc13c1c8494c
Lee <handsomeleesouth@qq.com>
2023-01-09 20:11:46 +0800

    设置读写分离

--- src/main/java/com/mt/reggie/ReggieApplication.java
+++ src/main/java/com/mt/reggie/ReggieApplication.java
@@ -5,13 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @Slf4j//是lombok中提供的注解, 用来通过slf4j记录日志,可以引入log
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})//配置
 @MapperScan("com.mt.reggie.mapper")//添加mapper扫描
 @ServletComponentScan//添加Servlet扫描的注解,可以扫描到自定义的filter类
 @EnableTransactionManagement//开启事务管理
